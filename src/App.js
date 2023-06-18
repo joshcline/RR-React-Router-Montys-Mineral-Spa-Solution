c306c1c4d9def380a02f6e7ddf0d244afb33fc65
Josh Cline <joshccline@outlook.com>
2023-06-18 00:24:02 -0400

    Added styling and restructered links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
-
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import './App.css';
 
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
 
-
 function App() {
   
   const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
-
 
   return (
     <div className="App">
@@ -18,19 +17,31 @@ function App() {
         <header>
           <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
-          </div>
+          <Container>
+            <Nav defaultActiveKey="/" variant="tabs" fill>
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/" eventKey={"homePage"}> 
+                        Home
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item >
+                  <Nav.Link as={Link} to="/about" eventKey={"aboutPage"}> 
+                      About Us
+                  </Nav.Link>
+                </Nav.Item>
+                {/* <Nav.Item >
+                    <Nav.Link eventKey={"aboutPage"}> 
+                        <Link to="/about">About Us</Link>
+                    </Nav.Link>
+                </Nav.Item> */}
+                <Nav.Item >
+                    <Nav.Link as={Link} to="/packages" eventKey={"packagesPage"}> 
+                        Our Packages 
+                    </Nav.Link>
+                </Nav.Item>
+            </Nav>
+          </Container>
+
         </header>
 
         <div className="display">
@@ -46,8 +57,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
